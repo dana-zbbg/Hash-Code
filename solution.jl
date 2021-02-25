47658ba5f0db0e741d35f646cb4d54d4d105b806
audreygossard <>
2021-02-25 20:07:28 +0100

    Update solution.jl

--- solution.jl
+++ solution.jl
@@ -1,5 +1,5 @@
 struct Schedule
-    A::Int
+    A::Int  #nb intersections
     intersections::Vector{Intersection}
 
     Schedule(; A, intersections) = new(A, intersections)
@@ -7,17 +7,17 @@ end
 
 
 struct Intersection
-    i::Int
-    E::Int
-    lights::Vector{Light}
+    i::Int  #id de l'intersection
+    E::Int  #nb de routes qui vont vers cette intersection
+    lights::Vector{Light}  #vecteur des routes et de leur duree de feu vert
 
     Intersection(; i, E, lights) = new(i, E, lights)
 end
 
 
 struct Light
-    street::String
-    T::Int
+    street::String  #nom de la route
+    T::Int  #duree de son feu vert
 
     Light(; street, T) = new(street, T)
 end
