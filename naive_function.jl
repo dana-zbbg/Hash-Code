942e3156995fa02858d6c1f21f3a855f80fa123a
Dana Zilberberg <dana.zilberberg@gmail.com>
2021-02-25 20:49:21 +0100

    solution naive

---
+++ naive_function.jl
@@ -0,0 +1,15 @@
+function naive(instance::Instance, T::Int )
+    intersections = Vector{Intersection}()
+    for i=1:Instance.I #pour chaque intersection
+        lights = Vector{Light}()
+        E = 0 #nb de incoming streets
+        for s in Instance.streets #on regarde si la route est une incoming street
+            if s.E == i #if s is an incoming street on i
+                E += 1
+                push!(lights, Light(s.name, T))
+            end
+        end
+        push!(instersections, Intersection(i = i, E = E, lights = lights))
+    end
+end
+          
