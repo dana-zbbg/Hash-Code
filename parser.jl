de83235bf13cbb553d7878f5fb0ea22482ae599e
audreygossard <>
2021-02-25 19:47:39 +0100

    Update parser.jl

--- parser.jl
+++ parser.jl
@@ -32,27 +32,27 @@ function lire_instance(path::String)::Instance
         readlines(file)
     end
 
-    dims = data[1]
-    D = dims[1]
-    I = dims[2]
-    S = dims[3]
-    V = dims[4]
-    F = dims[5]
+    dims = split(data[1], " ")
+    D = parse(Int, dims[1])
+    I = parse(Int, dims[2])
+    S = parse(Int, dims[3])
+    V = parse(Int, dims[4])
+    F = parse(Int, dims[5])
     streets = Vector{Street}()
     paths = Vector{Path}()
 
     for (i=2:S+1)
-        street = data[i]
-        B = street[1]
-        E = street[2]
+        street = split(data[i], " ")
+        B = parse(Int, street[1])
+        E = parse(Int, street[2])
         name = street[3]
-        L = street[4]
+        L = parse(Int, street[4])
         push!(streets, Street(B=B, E=E, name=name, L=L))
     end
 
     for (i=S+2:S+V+1)
-        path = data[i]
-        P = path[1]
+        path = split(data[i], " ")
+        P = parse(Int, path[1])
         street_names = Vector{String}()
         for (j=1:P)
             push!(street_names, path[1+j])
