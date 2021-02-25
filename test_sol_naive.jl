1c95ce8df7926156ac55c0c779bdbc1d55f384dc
audreygossard <>
2021-02-25 22:05:15 +0100

    solution txt

--- test_sol_naive.jl
+++ test_sol_naive.jl
@@ -4,60 +4,63 @@ include("naive_function.jl")
 
 
 path = joinpath(".", "a.txt")
-path_write_sol = joinpath(".", "solutions_naive/sol_a.txt")
+path_write_sol = joinpath(".", "solutions_naive2/sol_a.txt")
+
+
+T = 1
 
 instance = lire_instance(path)
-intersections = naive(instance, 2)
+intersections = naive(instance, T)
 schedule = Schedule(A = instance.I, intersections = intersections)
 
 write_solution(schedule, path_write_sol)
 
 
 path = joinpath(".", "b.txt")
-path_write_sol = joinpath(".", "solutions_naive/sol_b.txt")
+path_write_sol = joinpath(".", "solutions_naive2/sol_b.txt")
 
 instance = lire_instance(path)
-intersections = naive(instance, 2)
+intersections = naive(instance, T)
 schedule = Schedule(A = instance.I, intersections = intersections)
 
 write_solution(schedule, path_write_sol)
 
 
 path = joinpath(".", "c.txt")
-path_write_sol = joinpath(".", "solutions_naive/sol_c.txt")
+path_write_sol = joinpath(".", "solutions_naive2/sol_c.txt")
 
 instance = lire_instance(path)
-intersections = naive(instance, 2)
+intersections = naive(instance, T)
 schedule = Schedule(A = instance.I, intersections = intersections)
 
 write_solution(schedule, path_write_sol)
 
 
 path = joinpath(".", "d.txt")
-path_write_sol = joinpath(".", "solutions_naive/sol_d.txt")
+path_write_sol = joinpath(".", "solutions_naive2/sol_d.txt")
 
 instance = lire_instance(path)
-intersections = naive(instance, 2)
+intersections = naive(instance, T)
 schedule = Schedule(A = instance.I, intersections = intersections)
 
 write_solution(schedule, path_write_sol)
 
 
 path = joinpath(".", "e.txt")
-path_write_sol = joinpath(".", "solutions_naive/sol_e.txt")
+path_write_sol = joinpath(".", "solutions_naive2/sol_e.txt")
 
 instance = lire_instance(path)
-intersections = naive(instance, 2)
+intersections = naive(instance, T)
 schedule = Schedule(A = instance.I, intersections = intersections)
 
 write_solution(schedule, path_write_sol)
 
 
 path = joinpath(".", "f.txt")
-path_write_sol = joinpath(".", "solutions_naive/sol_f.txt")
+path_write_sol = joinpath(".", "solutions_naive2/sol_f.txt")
 
 instance = lire_instance(path)
-intersections = naive(instance, 2)
+intersections = naive(instance, T)
 schedule = Schedule(A = instance.I, intersections = intersections)
 
 write_solution(schedule, path_write_sol)
